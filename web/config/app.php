a5f13b283b3a83619e1804332326d9f60d38323a
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-06-21 11:58:11 -0400

    Add excel library

--- web/config/app.php
+++ web/config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
